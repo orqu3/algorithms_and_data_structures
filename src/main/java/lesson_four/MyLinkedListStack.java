865c58ebf40914d06d68d59f3a2ee2823129f4d1
orqu3 <marisergiv@gmail.com>
2021-03-26 17:41:20 +0300

    Solution for lesson four

---
+++ src/main/java/lesson_four/MyLinkedListStack.java
@@ -0,0 +1,33 @@
+package lesson_four;
+
+public class MyLinkedListStack<T> {
+
+    private MyLinkedList<T> ll = new MyLinkedList<>();
+
+    public void push(T item){
+        ll.insertFirst(item);
+    }
+
+    public T peek(){
+        return ll.getFirst();
+    }
+
+    public T pop(){
+        return ll.removeFirst();
+    }
+
+    public int size(){
+        return ll.size();
+    }
+
+    public boolean isEmpty(){
+        return ll.isEmpty();
+    }
+
+    @Override
+    public String toString() {
+        return ll.toString();
+    }
+
+
+}
